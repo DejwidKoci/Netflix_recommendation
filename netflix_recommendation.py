1328e77436c242feea41151db039bfb3653d0e64
DejwidKoci <s205915@sggw.edu.pl>
2023-04-13 21:32:48 +0100

    first lists

--- netflix_recommendation.py
+++ netflix_recommendation.py
@@ -19,3 +19,25 @@ tv = data[data['Content Type'] == 'TV Show'].reset_index()
 tv = tv.drop(['index', 'Show Id', 'Content Type', 'Date Added', 'Release Date', 'Duration', 'Description'], axix = 1)
 tv.head()
 
+actors = []
+for i in movies['Cast']:
+    actor = re.split(r', \s*', i)
+    actors.append(actor)
+
+flat_list = []
+for sublist in actors:
+    for item in sublist:
+        flat_list.append(item)
+
+actors_list = sorted(set(flat_list))
+binary_actors = [[0] * 0 for i in range(len(set(flat_list)))]
+for i in movies['Cast']:
+    k = 0
+    for j in actors_list:
+        if j in i:
+            binary_actors[k].append(1.0)
+        else:
+            binary_actors[k].append(0.0)
+        k += 1
+
+ 
