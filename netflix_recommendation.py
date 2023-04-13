cdf7aac2db9d0db43b2dcab98595d15d0b79c385
DejwidKoci <s205915@sggw.edu.pl>
2023-04-13 21:20:11 +0100

    movies and tv data

--- netflix_recommendation.py
+++ netflix_recommendation.py
@@ -6,3 +6,16 @@ import nltk
 import tkinter as tk
 from nltk.corpus import stopwords
 
+nltk.download('stopwords')
+
+data = pd.read_csv('netflixData.csv')
+data = data.dropna(subset = ['Cast', 'Production Country','Rating'])
+
+movies = data[data['Content Type'] == 'Moive'].reset_index()
+movies = movies.drop(['index', 'Show Id', 'Content Type', 'Date Added', 'Release Date', 'Duration', 'Description'], axix = 1)
+movies.head()
+
+tv = data[data['Content Type'] == 'TV Show'].reset_index()
+tv = tv.drop(['index', 'Show Id', 'Content Type', 'Date Added', 'Release Date', 'Duration', 'Description'], axix = 1)
+tv.head()
+
